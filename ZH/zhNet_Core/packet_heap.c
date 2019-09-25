98a88419d79e76e3f5a8cbda6703ef56050e30a9
han.zh <hzh.888@qq.com>
2019-09-25 14:45:57 +0800

    Signed-off-by: han.zh <hzh.888@qq.com>

--- ZH/zhNet_Core/packet_heap.c
+++ ZH/zhNet_Core/packet_heap.c
@@ -1,4 +1,4 @@
-/* 
+ï»¿/* 
   packet.h:the base data processed send to network
 */
 
@@ -27,7 +27,7 @@
 //
 //initizal read data
 //
-//³õÊ¼»¯¶ÁÈ¡Êý¾Ý
+//åˆå§‹åŒ–è¯»å–æ•°æ®
 //
 void zhPackHeapReadInit(TzhPacketHeap *p,unsigned char*szBuf,int nBufSize)
 {
@@ -58,58 +58,58 @@ void zhPackHeapFree(TzhPacketHeap *p)
 
 int zhPackHeapWriteChar(TzhPacketHeap *p, char i)
 {
-	if(p->wPos+sizeof(char)>p->nBufLenght){return 0;}
-	memcpy(&p->btBuf[p->wPos], &i, sizeof(char));
+	if((int)(p->wPos+sizeof(char))>p->nBufLenght){return 0;}
+	memcpy(&p->btBuf[(int)p->wPos], &i, sizeof(char));
 	p->wPos+=sizeof(char);
 	p->wSize+=sizeof(char);
 	return sizeof(char);
 }
 int zhPackHeapReadChar(TzhPacketHeap *p, char* i)
 {
-	memcpy(i, &p->btBuf[p->wPos], sizeof(char));
+	memcpy(i, &p->btBuf[(int)p->wPos], sizeof(char));
 	p->wPos+=sizeof(char);
 	return sizeof(char);
 }
 
 int zhPackHeapWriteShort(TzhPacketHeap *p, short i)
 {
-	if(p->wPos+sizeof(short)>p->nBufLenght){return 0;}
-	memcpy(&p->btBuf[p->wPos], &i, sizeof(short));
+	if((int)(p->wPos+sizeof(short))>p->nBufLenght){return 0;}
+	memcpy(&p->btBuf[(int)p->wPos], &i, sizeof(short));
 	p->wPos+=sizeof(short);
 	p->wSize+=sizeof(short);
 	return sizeof(short);
 }
 int zhPackHeapReadShort(TzhPacketHeap *p, short* i)
 {	
-	memcpy(i, &p->btBuf[p->wPos], sizeof(short));
+	memcpy(i, &p->btBuf[(int)p->wPos], sizeof(short));
 	p->wPos+=sizeof(short);
 	return sizeof(short);
 }
 int zhPackHeapWriteInt(TzhPacketHeap *p, int i)
 {
-	if(p->wPos+sizeof(int)>p->nBufLenght){return 0;}
-	memcpy(&p->btBuf[p->wPos], &i, sizeof(int));
+	if((int)(p->wPos+sizeof(int))>p->nBufLenght){return 0;}
+	memcpy(&p->btBuf[(int)p->wPos], &i, sizeof(int));
 	p->wPos+=sizeof(int);
 	p->wSize+=sizeof(int);
 	return sizeof(int);
 }
 int zhPackHeapReadInt(TzhPacketHeap *p, int* i)
 {
-	memcpy(i, &p->btBuf[p->wPos], sizeof(int));
+	memcpy(i, &p->btBuf[(int)p->wPos], sizeof(int));
 	p->wPos+=sizeof(int);
 	return sizeof(int);
 }
 int zhPackHeapWriteLong(TzhPacketHeap *p, long i)
 {
-	if(p->wPos+sizeof(long)>p->nBufLenght){return 0;}
-	memcpy(&p->btBuf[p->wPos], &i, sizeof(long));
+	if((int)(p->wPos+sizeof(long))>p->nBufLenght){return 0;}
+	memcpy(&p->btBuf[(int)p->wPos], &i, sizeof(long));
 	p->wPos+=sizeof(long);
 	p->wSize+=sizeof(long);
 	return sizeof(long);
 }
 int zhPackHeapReadLong(TzhPacketHeap *p, long* i)
 {
-	memcpy(i, &p->btBuf[p->wPos], sizeof(long));
+	memcpy(i, &p->btBuf[(int)p->wPos], sizeof(long));
 	p->wPos+=sizeof(long);
 	return sizeof(long);
 }
@@ -119,10 +119,10 @@ int zhPackHeapWriteString(TzhPacketHeap *p, char* str)
 	
 	len =strlen(str);
 
-	if(p->wPos+len>p->nBufLenght){return 0;}
+	if((int)(p->wPos+len)>p->nBufLenght){return 0;}
 
-	strcpy((char*)&p->btBuf[p->wPos], str);
-	p->btBuf[len + p->wPos] =0;
+	strcpy((char*)&p->btBuf[(int)p->wPos], str);
+	p->btBuf[len + (int)p->wPos] =0;
 	len += 1;
 	
 	p->wPos+=(unsigned short)len;
@@ -137,11 +137,11 @@ int zhPackHeapWriteStringUnicode(TzhPacketHeap *p, char* str)
 	while(str[len]!=0 || str[len+1]!=0)
 	{len+=2;}
 
-	if(p->wPos+len>p->nBufLenght){return 0;}
+	if((int)(p->wPos+len)>p->nBufLenght){return 0;}
 
-	memcpy(&p->btBuf[p->wPos], str,len);
-	p->btBuf[p->wPos+len] = 0;
-	p->btBuf[p->wPos+len+1] = 0;
+	memcpy(&p->btBuf[(int)p->wPos], str,len);
+	p->btBuf[(int)p->wPos+len] = 0;
+	p->btBuf[(int)p->wPos+len+1] = 0;
 	len += 2;
 
 	p->wPos+=(unsigned short)len;
@@ -153,8 +153,8 @@ int zhPackHeapReadString(TzhPacketHeap *p, char* str)
 {
 	size_t len=0;
 
-	len = strlen((char*)&p->btBuf[p->wPos]);
-	strcpy(str, (char*)&p->btBuf[p->wPos]);
+	len = strlen((char*)&p->btBuf[(int)p->wPos]);
+	strcpy(str, (char*)&p->btBuf[(int)p->wPos]);
 	str[len] = 0;
 	len += 1;
 
@@ -166,7 +166,7 @@ int	zhPackHeapReadStringN(TzhPacketHeap *p, char* str ,int strMaxLen)
 {
 	size_t len=0;
 	str[strMaxLen-1]=0;
-	strncpy(str, (char*)&p->btBuf[p->wPos],strMaxLen-2);
+	strncpy(str, (char*)&p->btBuf[(int)p->wPos],strMaxLen-2);
 	len = strlen(str);
 	str[len] = 0;
 	len += 1;
@@ -179,10 +179,10 @@ int zhPackHeapReadStringUnicode(TzhPacketHeap *p, char* str)
 {
 	size_t len=0;
 
-	while(p->btBuf[p->wPos+len]!=0 || p->btBuf[p->wPos+len+1]!=0)
+	while(p->btBuf[(int)p->wPos+len]!=0 || p->btBuf[(int)p->wPos+len+1]!=0)
 	{len+=2;}
 
-	memcpy(str,&p->btBuf[p->wPos], len);
+	memcpy(str,&p->btBuf[(int)p->wPos], len);
 	str[len] = 0;
 	str[len+1] = 0;
 	len += 2;
@@ -193,8 +193,8 @@ int zhPackHeapReadStringUnicode(TzhPacketHeap *p, char* str)
 
 int zhPackHeapWriteBinary(TzhPacketHeap *p, char* str ,int len)
 {
-	if(p->wPos+len>p->nBufLenght || len<0){return 0;}
-	memcpy(&p->btBuf[p->wPos], str,len);
+	if((int)p->wPos+len>p->nBufLenght || len<0){return 0;}
+	memcpy(&p->btBuf[(int)p->wPos], str,len);
 	p->wPos+=len;
 	p->wSize+=len;
 	return len;
@@ -202,22 +202,22 @@ int zhPackHeapWriteBinary(TzhPacketHeap *p, char* str ,int len)
 int zhPackHeapReadBinary(TzhPacketHeap *p, char* str ,int len)
 {
 	if(len<0 || len>p->nBufLenght){return 0;}
-	memcpy(str, &p->btBuf[p->wPos],len);
+	memcpy(str, &p->btBuf[(int)p->wPos],len);
 	p->wPos+=len;
 	return len;
 }
 int zhPackHeapWriteBool(TzhPacketHeap *p,bool b)
 {
 	char a=b?1:0;
-	if(p->wPos+1>p->nBufLenght){return 0;}
-	memcpy(&p->btBuf[p->wPos], &a, 1);
+	if((int)p->wPos+1>p->nBufLenght){return 0;}
+	memcpy(&p->btBuf[(int)p->wPos], &a, 1);
 	p->wPos+=1;
 	p->wSize+=1;
 	return 1;
 }
 int zhPackHeapReadBool(TzhPacketHeap *p, bool*b)
 {
-	memcpy(b, &p->btBuf[p->wPos], 1);
+	memcpy(b, &p->btBuf[(int)p->wPos], 1);
 	p->wPos+=1;
 	return 1;
 }
@@ -226,58 +226,58 @@ int zhPackHeapReadBool(TzhPacketHeap *p, bool*b)
 ////////////////////////////
 int zhPackHeapWriteUnsignedChar(TzhPacketHeap *p,unsigned char i)
 {
-	if(p->wPos+1>p->nBufLenght){return 0;}
-	memcpy(&p->btBuf[p->wPos], &i, 1);
+	if((int)p->wPos+1>p->nBufLenght){return 0;}
+	memcpy(&p->btBuf[(int)p->wPos], &i, 1);
 	p->wPos+=1;
 	p->wSize+=1;
 	return 1;
 }
 int zhPackHeapReadUnsignedChar(TzhPacketHeap *p,unsigned char* i)
 {
-	memcpy(i, &p->btBuf[p->wPos], sizeof(unsigned char));
+	memcpy(i, &p->btBuf[(int)p->wPos], sizeof(unsigned char));
 	p->wPos+=1;
 	return 1;
 }
 
 int zhPackHeapWriteUnsignedShort(TzhPacketHeap *p,unsigned short i)
 {
-	if(p->wPos+2>p->nBufLenght){return 0;}
-	memcpy(&p->btBuf[p->wPos], &i, 2);
+	if((int)p->wPos+2>p->nBufLenght){return 0;}
+	memcpy(&p->btBuf[(int)p->wPos], &i, 2);
 	p->wPos+=2;
 	p->wSize+=2;
 	return 2;
 }
 int zhPackHeapReadUnsignedShort(TzhPacketHeap *p,unsigned short* i)
 {
-	memcpy(i, &p->btBuf[p->wPos], 2);
+	memcpy(i, &p->btBuf[(int)p->wPos], 2);
 	p->wPos+=2;
 	return 2;
 }
 int zhPackHeapWriteUnsignedInt(TzhPacketHeap *p,unsigned int i)
 {
-	if(p->wPos+4>p->nBufLenght){return 0;}
-	memcpy(&p->btBuf[p->wPos], &i, 4);
+	if((int)p->wPos+4>p->nBufLenght){return 0;}
+	memcpy(&p->btBuf[(int)p->wPos], &i, 4);
 	p->wPos+=4;
 	p->wSize+=4;
 	return 4;
 }
 int zhPackHeapReadUnsignedInt(TzhPacketHeap *p,unsigned int* i)
 {
-	memcpy(i, &p->btBuf[p->wPos], 4);
+	memcpy(i, &p->btBuf[(int)p->wPos], 4);
 	p->wPos+=4;
 	return 4;
 }
 int zhPackHeapWriteUnsignedLong(TzhPacketHeap *p,unsigned long i)
 {
-	if(p->wPos+8>p->nBufLenght){return 0;}
-	memcpy(&p->btBuf[p->wPos], &i, 8);
+	if((int)p->wPos+8>p->nBufLenght){return 0;}
+	memcpy(&p->btBuf[(int)p->wPos], &i, 8);
 	p->wPos+=8;
 	p->wSize+=8;
 	return 8;
 }
 int zhPackHeapReadUnsignedLong(TzhPacketHeap *p,unsigned long* i)
 {
-	memcpy(i, &p->btBuf[p->wPos], 8);
+	memcpy(i, &p->btBuf[(int)p->wPos], 8);
 	p->wPos+=8;
 	return 8;
 }
